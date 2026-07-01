b40c416bcc1c05061dd1887052158d6132d83d3f
Vadim Mitko <vmmmvmm@icloud.com>
2026-07-01 11:17:22 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_set>
 #include <string>
 
 int main() {
@@ -6,5 +7,17 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
+  std::string userCmd;
+  std::unordered_set<std::string> cmds;
+
   std::cout << "$ ";
+
+  // read user's input
+  std::getline(std::cin, userCmd);
+
+  // Print an error message in exactly this format: {command}: command not found
+  if (!cmds.count(userCmd)) {
+	  std::cout << userCmd << ": command not found\n";
+  }
+
 }
