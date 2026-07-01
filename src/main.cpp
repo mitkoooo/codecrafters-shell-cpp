80fc6508e1a08285c58af9242d02bf56160e6c33
Vadim Mitko <vmmmvmm@icloud.com>
2026-07-01 11:51:20 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 #include <functional>
 #include <string>
+#include <sstream>
+
+
+std::pair<std::string, std::vector<std::string>> parseInput(std::string line) {
+	std::stringstream ss;
+	std::string cmd;
+	std::vector<std::string> args;
+
+	ss >> cmd;
+	std::string word;
+
+	while (ss >> word) {
+		args.push_back(word);
+	}
+
+	return {cmd, args};
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::unordered_map<std::string, std::function<void()>> cmds;
+  std::unordered_map<std::string, std::function<void(std::vector<std::string>)>> cmds;
 
-  cmds["exit"] = []() {
+  cmds["exit"] = [](const std::vector<std::string>& args) {
 	exit(0);	
   };
 
+
   while (true) {
-	  std::string userCmd;
+	  std::string line;
 	  std::cout << "$ ";
 
 	  // read user's input
-	  std::getline(std::cin, userCmd);
+	  std::getline(std::cin, line);
+
+	  if (line.empty()) continue;
+
+	  auto [cmd, args] = parseInput(line);
 
 	  // Print an error message in exactly this format: {command}: command not found
-	  if (!cmds.count(userCmd)) {
-		  std::cerr << userCmd << ": command not found\n";
+	  if (!cmds.count(cmd)) {
+		  std::cerr << cmd << ": command not found\n";
 		  continue;
 	  }
 
-	  cmds[userCmd]();
+	  cmds[cmd](args);
   }
 
 }
